14f3e4229466a60f151fa99ca9eff9cd3c717479
Rez Delamasa <rezdelamasa@gmail.com>
2019-08-20 12:20:20 -0500

    Updates

---
+++ precache-manifest.7e2930ef955b08e4cd646dc767ff4625.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c94dd0eb3ca9c602c64ae75a609015",
+    "url": "/apex-legend/index.html"
+  },
+  {
+    "revision": "408de8af5e1ab6f96d63",
+    "url": "/apex-legend/static/css/main.14ebbea4.chunk.css"
+  },
+  {
+    "revision": "2dc605153f3afd8fa261",
+    "url": "/apex-legend/static/js/2.3e7dde31.chunk.js"
+  },
+  {
+    "revision": "408de8af5e1ab6f96d63",
+    "url": "/apex-legend/static/js/main.1be7070a.chunk.js"
+  },
+  {
+    "revision": "afe79aed07cc184dff38",
+    "url": "/apex-legend/static/js/runtime~main.1bbd0c44.js"
+  }
+]);
